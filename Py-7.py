f8d5eed513ff1db3f4bd4f204a6b2a8c36b05531
GreLukas <95912972+GreLukas@users.noreply.github.com>
2022-11-08 09:53:33 +0100

    Add files via upload

---
+++ Py-7.py
@@ -0,0 +1,34 @@
+#1
+from turtle import *
+import turtle
+ 
+tur = turtle.Turtle()
+ 
+tur.forward(100)  
+tur.left(120)
+tur.forward(100)
+ 
+tur.left(120)
+tur.forward(100)
+ 
+turtle.done()
+
+#2
+import turtle
+skk = turtle.Turtle()
+ 
+for i in range(4):
+    skk.forward(100)
+    skk.right(90)
+     
+turtle.done()
+
+#3
+from turtle import Turtle
+julie = Turtle()
+
+def polygon(n, side):
+    pass
+
+polygon(3, 180)
+polygon(5, 100)
